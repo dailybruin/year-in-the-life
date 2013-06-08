ff20f5a80eef319fecb77f7b999f290a3d4ee03f
Byron Lutz <byronlutz@gmail.com>
2013-06-08 16:24:17 -0700

    Intro page is styled

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+	$('#intro').css('height',window.innerHeight);
+	$('#intro #info-name').focus();
 
+	$('#info-form').bind("keypress", function(e) {
+		var code = e.keyCode || e.which; 
+		if (code  == 13) {               
+			e.preventDefault();
+			return false;
+		}
+	});
+});
